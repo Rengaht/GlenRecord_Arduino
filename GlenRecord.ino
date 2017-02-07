5529cfaced932508eedbe5097f4fb0486be4da55
Reng Tsai <rengah0@gmail.com>
2017-02-07 12:27:29 +0800

    update
    - long-press trigger when button down
    - add 7-press

--- GlenRecord.ino
+++ GlenRecord.ino
@@ -1,6 +1,8 @@
 #define SHORT_PRESS 10
-#define LONG_PRESS 50
-#define LONG_LONG_PRESS 100
+#define LONG_PRESS 60
+#define LONG_LONG_PRESS 500
+#define SHORT_INTER 40
+
 const int RecLightPin=7;
 const int RecPin=5;
 const int PlayPin=4;
@@ -13,7 +15,12 @@ bool play_mode=false;
 int rec_pressed_time=0;
 int play_pressed_time=0;
 
-// send 1: rec, 2: play, 3: check, 4: reset
+int suprise_count=0;
+int inter_time=0;
+
+int play_long_delay=0;
+
+// send 1: rec, 2: play, 3: check, 4: reset, 7:suprise
 
 
 void setup(){
@@ -29,27 +36,56 @@ void setup(){
 
 void loop(){
 
- 
-  
-  if(checkReset()){
-    delay(2000);
-    return;
-  }
   
   if(digitalRead(RecPin)==HIGH){    
     rec_pressed_time++;
-  }else{    
-    if(rec_pressed_time>SHORT_PRESS) Serial.println("1|");     
+    if(checkReset()){
+      delay(2000);         
+      return;
+    }
+    
+  }else{  
+    if(rec_pressed_time>SHORT_PRESS){      
+      
+      if(inter_time<SHORT_INTER){
+        suprise_count++;    
+        
+        if(suprise_count>=6){
+          Serial.println("7|");
+          suprise_count=0;
+          delay(2000);
+        }    
+      }else{
+        Serial.println("1|");         
+        suprise_count=0;
+      }
+
+      inter_time=0;
+      
+    }
+    
     rec_pressed_time=0;
   }
+
+  
   
   if(digitalRead(PlayPin)==HIGH){
-    play_pressed_time++;
+    play_pressed_time++;    
+
+    if(play_pressed_time>LONG_PRESS && digitalRead(RecPin)==LOW){
+        Serial.println("3|");      
+//        play_long_delay=LONG_PRESS;
+        play_pressed_time=0; 
+        suprise_count=0;       
+        delay(2000);         
+    }
   }else{
-    if(play_pressed_time>LONG_PRESS) Serial.println("3|");      
-    else if(play_pressed_time>SHORT_PRESS) Serial.println("2|");         
-    
+    if(play_pressed_time>SHORT_PRESS){
+        Serial.println("2|");             
+       suprise_count=0;
+    }
     play_pressed_time=0;    
+    
   }
   
   if(Serial.available()){
@@ -65,15 +101,19 @@ void loop(){
     }
   }
   analogWrite(LightPin,light_val);
-    
+
+  inter_time++;
+  if(play_long_delay>0) play_long_delay--;
+  
   delay(10);
   
 }
 
 bool checkReset(){
-  if(play_pressed_time>LONG_LONG_PRESS && rec_pressed_time>LONG_LONG_PRESS){
-    Serial.println("4|");    
-    play_pressed_time=rec_pressed_time=0;    
+  if(rec_pressed_time>LONG_LONG_PRESS && play_pressed_time>LONG_LONG_PRESS){ 
+      Serial.println("4|");    
+      play_pressed_time=rec_pressed_time=0;    
+      suprise_count=0;      
     return true;
   }
   return false;
